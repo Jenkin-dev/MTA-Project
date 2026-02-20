c8c0f080ecdb52edd6f56e7c771862228bf1846f
C_Jenkins <gbengachris87@gmail.com>
2026-02-20 13:59:31 +0100

    updated contact form

--- links/contact.js
+++ links/contact.js
@@ -2,13 +2,17 @@ const form = document.getElementById("contactForm");
 
 form.addEventListener("submit", function (event) {
   // Check if the form is valid based on the 'required' attributes
+
+  const formData = new FormData(form);
+  const userName = formData.get("Fullname");
+
+  event.preventDefault();
   if (!form.checkValidity()) {
-    // If not valid, browser shows default error messages
-    event.preventDefault();
+    alert(`${userName} make sure to fill the form properly`);
   } else {
-    // If valid, prevent default form action and redirect manually
-    event.preventDefault();
-    alert("Thanks for contacting JenAuto! Redirecting to Home...");
+    alert(
+      `Thank you, ${userName} for contacting JenAuto! Redirecting to Home...`,
+    );
     window.location.href = "../index.html";
   }
 });
